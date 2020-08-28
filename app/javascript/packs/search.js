34915ae255d24eb628671711c6838b00573497bf
Remy Menard <remymenard.work@gmail.com>
2020-08-28 15:38:31 +0200

    fixed script qui fairt tout buger

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,24 +1,24 @@
-document.addEventListener('turbolinks:load', () => displayWords());
+// document.addEventListener('turbolinks:load', () => displayWords());
 
-const results = document.querySelector('.words');
+// const results = document.querySelector('.words');
 
-const input = document.querySelector('.search');
+// const input = document.querySelector('.search');
 
-const drawResponseList = (data) => {
-  results.innerHTML = '';
-  data.words.forEach((word) => {
-    results.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
-  });
-};
+// const drawResponseList = (data) => {
+//   results.innerHTML = '';
+//   data.words.forEach((word) => {
+//     results.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
+//   });
+// };
 
-const autocomplete = (e) => {
-  fetch('@words_path')
-    .then(response => response.json())
-    .then(data => drawResponseList(data));
-};
-const displayWords = (event) => {
-  input.addEventListener('keyup', autocomplete);
+// const autocomplete = (e) => {
+//   fetch('@words_path')
+//     .then(response => response.json())
+//     .then(data => drawResponseList(data));
+// };
+// const displayWords = (event) => {
+//   input.addEventListener('keyup', autocomplete);
 
-}
+// }
  // méthode qui me renvoit tout les mots qui commence par ce qui est écrit -> dans le controller (méthode ? show)
  //
