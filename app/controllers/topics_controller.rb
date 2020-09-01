42eceb75267d6234edc22340e8e38c31ed23f7e7
Remy Menard <remymenard.work@gmail.com>
2020-09-01 10:45:56 +0200

    added the possibility to select your topics and validations

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,15 @@
+class TopicsController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def post
+    @user = current_user
+    selected_params = Topic::NAMES.select do |name|
+      params[name.downcase] == "true"
+    end
+    @user.topics = selected_params
+    @user.save!
+    redirect_to articles_path
+  end
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
   has_many :articles
-
+  NAMES = ['technology', 'future', 'health', 'work', 'science', 'business', 'culture', 'food', 'programming', 'design', 'politics', 'human', 'self', 'startups']
   validates :name, presence: true, allow_blank: false
 end
