e11bc424cde4f50f0fb09a2a886a21d12b3f1475
Remy Menard <remymenard.work@gmail.com>
2020-09-04 17:47:48 +0200

    apple

--- app/services/words/new_service.rb
+++ app/services/words/new_service.rb
@@ -7,7 +7,7 @@ module Words
 
     def call
       Word.create(user: @user, word: 'supply', translation: 'fourniture', last_review: @now)
-      Word.create(user: @user, word: 'take over', translation: 'reprendre', last_review: @now)
+      Word.create(user: @user, word: 'apple', translation: 'pomme', last_review: @now)
       Word.create(user: @user, word: 'lukewarm', translation: 'tiède', last_review: @now)
     end
   end
